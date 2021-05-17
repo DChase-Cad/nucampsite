493d12d4171adef60fcce5f49b8e19db68a81909
Dane Chase <danechase@gmail.com>
2021-05-17 16:22:06 -0500

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -32,7 +32,6 @@ class Main extends Component {
                     <Route exact path='/directory' render={()=> <Directory campsites={this.state.campsites} />} />
                     <Redirect to='/home' />
                 </Switch>
-                <Directory campsites={this.state.campsites}/>
                 <Footer />
               
             </div>
